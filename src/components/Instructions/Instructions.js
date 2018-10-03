6d0b3622992c8c629b54a8986970bb7f60449428
Evan Tucker <eej.tucker@gmail.com>
2018-10-03 10:41:45 -0600

    changed instruction image

--- src/components/Instructions/Instructions.js
+++ src/components/Instructions/Instructions.js
@@ -4,7 +4,7 @@ import "./Instructions.css";
 
 const Instructions = (props) => (
     <div className="jumbo">
-        <Jumbotron style={{'background-image': `url("/assets/images/mario-background.gif")`}} >
+        <Jumbotron style={{'background-image': `url("/assets/images/checkered-flag.svg")`}} >
             <h3 className="instructions">{props.message}</h3>
         </Jumbotron>
     </div>
